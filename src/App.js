6b93b33a3be3e9c0c88a73cb2738c77992b49193
Julio Noguera <noguerajulio24@gmail.com>
2020-02-08 23:21:31 -0500

    ahora si responsive

--- src/App.js
+++ src/App.js
@@ -63,13 +63,13 @@ function App() {
               (
                 <>
                   <div className="row">
-                    <div className="col-6 col-sm-6">
+                    <div className="col-12 col-sm-12 col-md-6">
                       <Formulario
                         guardarGasto={guardarGasto}
                         guardarCrearGasto={guardarCrearGasto}
                       />
                     </div>
-                    <div className="col-6 col-sm-6">
+                    <div className="col-12 col-sm-12 col-md-6">
                       <Listado 
                         gastos={gastos}
                       />
